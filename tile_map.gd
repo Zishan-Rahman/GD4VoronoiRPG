9f1b206f323ed7271643e9f0b62363ccbece02d2
Zishan Rahman <zishanfrahman@outlook.com>
2023-04-12 18:34:17 +0100

    Add buildings to world generation

--- tile_map.gd
+++ tile_map.gd
@@ -1,10 +1,55 @@
 extends TileMap
 
+var buildings: Array[Vector2i] = [
+	Vector2i(0, 19),
+	Vector2i(1, 19),
+	Vector2i(2, 19),
+	Vector2i(3, 19),
+	Vector2i(4, 19),
+	Vector2i(5, 19),
+	Vector2i(6, 19),
+	Vector2i(7, 19),
+	Vector2i(8, 20),
+	Vector2i(0, 20),
+	Vector2i(1, 20),
+	Vector2i(2, 20),
+	Vector2i(3, 20),
+	Vector2i(4, 20),
+	Vector2i(5, 20),
+	Vector2i(6, 20),
+	Vector2i(7, 20),
+	Vector2i(8, 20),
+	Vector2i(0, 21),
+	Vector2i(1, 21),
+	Vector2i(2, 21),
+	Vector2i(3, 21),
+	Vector2i(4, 21),
+	Vector2i(5, 21),
+	Vector2i(6, 21),
+	Vector2i(7, 21),
+	Vector2i(8, 21)
+]
+var trees: Array[Vector2i] = [
+	Vector2i(0,1),
+	Vector2i(1,1),
+	Vector2i(2,1),
+	Vector2i(3,1),
+	Vector2i(4,1),
+	Vector2i(5,1),
+	Vector2i(6,1),
+	Vector2i(7,1),
+	Vector2i(0,2),
+	Vector2i(1,2),
+	Vector2i(2,2),
+	Vector2i(3,2),
+	Vector2i(4,2)
+]
+
 var points: Array[Dictionary] = []
 const EUCLIDEAN: String = "Euclidean distance"
 const MANHATTAN: String = "Manhattan distance"
 @export_enum(EUCLIDEAN, MANHATTAN) var distance: String = MANHATTAN
-@export_range(2, 6, 1) var random_starting_points: int = 4
+@export_range(10, 40, 1) var random_starting_points: int = 20
 var x_tile_range: int = ProjectSettings.get_setting("display/window/size/viewport_width") / tile_set.tile_size.x
 var y_tile_range: int = ProjectSettings.get_setting("display/window/size/viewport_height") / tile_set.tile_size.y
 
@@ -35,7 +80,8 @@ func calculate_points_delta(x: int, y: int, p: int) -> float:
 	return abs(points[p]["x"] - x) + abs(points[p]["y"] - y)
 
 func define_points(num_points: int) -> void:
-	var types: Array[Vector2i] = [Vector2i(0,1),Vector2i(1,1),Vector2i(2,1),Vector2i(5,1),Vector2i(6,1),Vector2i(4,2)]
+	var types: Array[Vector2i] = trees.duplicate()
+	types.append_array(buildings)
 	for i in range(num_points):
 		var x: int = randi_range(0, x_tile_range)
 		var y: int = randi_range(0, y_tile_range)
