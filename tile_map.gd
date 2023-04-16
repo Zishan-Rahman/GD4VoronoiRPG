46d22c52366bf964a53bf2462aa685a6d522eab5
Zishan Rahman <zishanfrahman@outlook.com>
2023-04-16 23:57:52 +0100

    Include movement and other functions from other implementations and start work on adapting them accordingly
    TODO: Finish said work on adapting these functions to work with our Voronoï Cells implementation

--- tile_map.gd
+++ tile_map.gd
@@ -46,6 +46,13 @@ const trees: Array[Vector2i] = [
 ]
 const PLAYER_SPRITE: Vector2i = Vector2i(24, 7)
 var player_placement_cell: Vector2i
+const rings: Array[Vector2i] = [
+	Vector2i(43, 6),
+	Vector2i(44, 6),
+	Vector2i(45, 6),
+	Vector2i(46, 6)
+]
+var ring_placement_cell: Vector2i
 
 var points: Array[Dictionary] = []
 const EUCLIDEAN: String = "Euclidean distance"
@@ -61,8 +68,61 @@ func _ready() -> void:
 	var start_time: float = Time.get_ticks_msec()
 	define_points(random_starting_points)
 	paint_points()
+	place_player()
+	place_ring()
 	var new_time: float = Time.get_ticks_msec() - start_time
 	print("Time taken: " + str(new_time) + "ms")
+	$AcceptDialog.dialog_text = "You're a hollow Golem who seeks the ultimate treasure; a ring that's got something on top of it. It's somewhere in this large village and barely visible to your naked eyes, which took us " + str(new_time) + " milliseconds to generate (" + str(new_time / 1000.0) + " seconds), but you'll stop at nothing to get what you want. You can chow down every tree and fauna that stands in your way of the ring, but your Achilles heel is any bricks and mortar, which WILL make you stop at your tracks. Since it's easy to get lost in here, we'll tell you that you're in position " + str(player_placement_cell) + " in this big village of size " + str(Vector2i(x_tile_range, y_tile_range)) + ". However, it is YOUR job to find the ring, so are you ready to attain the treasure that is rightfully yours?!"
+	$AcceptDialog.visible = true
+	$AcceptDialog.confirmed.connect(_on_AcceptDialog_closed)
+	$AcceptDialog.canceled.connect(_on_AcceptDialog_closed)
+	$WinDialog.confirmed.connect(_on_WinDialog_confirmed)
+	$WinDialog.canceled.connect(_on_WinDialog_canceled)
+	get_tree().paused = true
+
+func _on_WinDialog_confirmed() -> void:
+	get_tree().reload_current_scene()
+
+func _on_WinDialog_canceled() -> void:
+	get_tree().quit()
+
+func _on_AcceptDialog_closed() -> void:
+	$AcceptDialog.visible = false
+	get_tree().paused = false
+
+func _get_random_placement_cell() -> Vector2i:
+	return Vector2i(randi() % x_tile_range, randi() % y_tile_range)
+
+func place_player() -> void:
+	player_placement_cell = _get_random_placement_cell()
+	while buildings.has(get_cell_atlas_coords(0, player_placement_cell)) or player_placement_cell == ring_placement_cell:
+		player_placement_cell = _get_random_placement_cell()
+	set_cell(0, player_placement_cell, 0, PLAYER_SPRITE)
+
+func place_ring() -> void:
+	ring_placement_cell = _get_random_placement_cell()
+	while buildings.has(get_cell_atlas_coords(0, ring_placement_cell)) or ring_placement_cell == player_placement_cell:
+		ring_placement_cell = _get_random_placement_cell()
+	set_cell(0, ring_placement_cell, 0, rings.pick_random())
+
+func _is_not_out_of_bounds(cell: Vector2i) -> bool:
+	return cell.x >= 0 and cell.x < x_tile_range and cell.y >= 0 and cell.y < y_tile_range
+
+func _physics_process(_delta) -> void:
+	var previous_cell: Vector2i = player_placement_cell
+	var direction: Vector2i = Vector2i.ZERO
+	if Input.is_action_pressed("ui_up"): direction = Vector2i.UP
+	elif Input.is_action_pressed("ui_down"): direction = Vector2i.DOWN
+	elif Input.is_action_pressed("ui_left"): direction = Vector2i.LEFT
+	elif Input.is_action_pressed("ui_right"): direction = Vector2i.RIGHT
+	var new_placement_cell: Vector2i = player_placement_cell + direction
+	if (not get_used_cells(0).has(new_placement_cell) or trees.has(get_cell_atlas_coords(0, new_placement_cell)) or new_placement_cell == ring_placement_cell) and _is_not_out_of_bounds(new_placement_cell):
+		player_placement_cell = new_placement_cell
+		set_cell(0, previous_cell, 0) # deletes contents of previous cell (atlas_coords = Vector2i(-1, -1))
+		set_cell(0, player_placement_cell, 0, PLAYER_SPRITE)
+		if player_placement_cell == ring_placement_cell:
+			$WinDialog.visible = true
+			get_tree().paused = true
 
 # ALGORITHM BEGINS HERE
 
