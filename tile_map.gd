6b8a9a8bd63ef159f662ec9a65e8864fc22c926d
Zishan Rahman <zishanfrahman@outlook.com>
2023-04-17 14:48:38 +0100

    Separate win condition and effects from _physics_process with new _win_game_if_won function
    Not needed in other implementations (but can be done nonetheless)

--- tile_map.gd
+++ tile_map.gd
@@ -121,15 +121,19 @@ func _physics_process(_delta) -> void:
 			player_placement_cell = _get_random_placement_cell()
 		set_cell(0, player_placement_cell, 0, PLAYER_SPRITE)
 		set_cell(0, previous_cell, 0) # replace the previous sprite
+		_win_game_if_won()
 		return
 	var new_placement_cell: Vector2i = player_placement_cell + direction
 	if (not get_used_cells(0).has(new_placement_cell) or trees.has(get_cell_atlas_coords(0, new_placement_cell)) or new_placement_cell == ring_placement_cell) and _is_not_out_of_bounds(new_placement_cell):
 		player_placement_cell = new_placement_cell
 		set_cell(0, previous_cell, 0) # deletes contents of previous cell (atlas_coords = Vector2i(-1, -1))
 		set_cell(0, player_placement_cell, 0, PLAYER_SPRITE)
-		if player_placement_cell == ring_placement_cell:
-			$WinDialog.visible = true
-			get_tree().paused = true
+		_win_game_if_won()
+
+func _win_game_if_won() -> void:
+	if player_placement_cell == ring_placement_cell:
+		$WinDialog.visible = true
+		get_tree().paused = true
 
 # ALGORITHM BEGINS HERE
 
